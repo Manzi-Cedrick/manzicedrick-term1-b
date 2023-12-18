ddd7cb9a334f42c911665f370b7814cfa7fd548a
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-12-18 11:45:35 +0200

    [perf]: request runtime exception --handling

--- src/main/java/com/example/demo/controller/MathOperandController.java
+++ src/main/java/com/example/demo/controller/MathOperandController.java
@@ -1,11 +1,10 @@
 package com.example.demo.controller;
 import com.example.demo.dto.DoMathRequestDto;
 import com.example.demo.exceptions.InvalidOperationException;
-import com.example.demo.services.IMathOperatorService;
+import com.example.demo.services.impls.MathOperatorServiceImpl;
 import com.example.demo.utils.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class MathOperandController {
-    @Autowired
-    private IMathOperatorService mathOperatorService;
+    private final MathOperatorServiceImpl mathOperatorService;
+
     @PostMapping("/do_math")
     public ResponseEntity<ApiResponse> create(@RequestBody DoMathRequestDto doMathRequestDto) throws InvalidOperationException {
-        return ResponseEntity.ok(ApiResponse.success(mathOperatorService.doMath(doMathRequestDto.getOperand1(), doMathRequestDto.getOperand2(), doMathRequestDto.getOperation())));
+        try {
+            double result = mathOperatorService.doMath(doMathRequestDto.getOperand1(), doMathRequestDto.getOperand2(), doMathRequestDto.getOperation());
+            return ResponseEntity.ok(ApiResponse.success(result));
+        } catch (RuntimeException e) {
+            return ResponseEntity.ok(ApiResponse.fail("An error occurred during the math operation."));
+        }
     }
 }
