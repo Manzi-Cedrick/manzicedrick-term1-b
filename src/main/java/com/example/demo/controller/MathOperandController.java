ff4e4c7aab81a6e2dbc2f91aa013b54da94585ea
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2023-12-18 09:48:41 +0200

    [perf]: controller functionality --addition

---
+++ src/main/java/com/example/demo/controller/MathOperandController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+import com.example.demo.dto.DoMathRequestDto;
+import com.example.demo.exceptions.InvalidOperationException;
+import com.example.demo.services.IMathOperatorService;
+import com.example.demo.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class MathOperandController {
+    private IMathOperatorService mathOperatorService;
+    @PostMapping("/do_math")
+    public ResponseEntity<ApiResponse> create(@RequestBody DoMathRequestDto doMathRequestDto) throws InvalidOperationException {
+        return ResponseEntity.ok(ApiResponse.success(mathOperatorService.doMath(doMathRequestDto.getOperand1(), doMathRequestDto.getOperand2(), doMathRequestDto.getOperation())));
+    }
+}
